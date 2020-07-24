b15e8d3eea5f521580d8a9f52d9a84c92bf9798f
arokokko <56476810+arokokko@users.noreply.github.com>
2020-07-24 22:46:15 +0700

    Create functions and gitignore file
    Create functions, create question to customers about favorite genres,
    .gitignore file create

--- script.js
+++ script.js
@@ -33,20 +33,44 @@
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-let numberOfFilms = +prompt('How many movies have you watched yet?', '');
-
-if (numberOfFilms <= 10 && numberOfFilms > 0) {
-    console.log('You have watched quite a few films');
-} else if (numberOfFilms > 10 && numberOfFilms <= 30 ) {
-    console.log('You are classic viewer');
-} else if (numberOfFilms > 30) {
-    console.log('You are movie fan');
-} else {
-    console.log('Oops, something going wrong!');
+let numberOfFilms;
+function moviesNumber() {
+    numberOfFilms  = +prompt('How many movies have you watched yet?', '');
+    while(numberOfFilms == '' || !numberOfFilms || isNaN(numberOfFilms)) {
+        numberOfFilms  = +prompt('How many movies have you watched yet?', '');
+    }
 }
+
+moviesNumber();
+
+function viewerLevel() {
+    if (numberOfFilms <= 10 && numberOfFilms > 0) {
+    console.log('You have watched quite a few films');
+    } else if (numberOfFilms > 10 && numberOfFilms <= 30 ) {
+        console.log('You are classic viewer');
+    } else if (numberOfFilms > 30) {
+        console.log('You are movie fan');
+    } else {
+        console.log('Oops, something going wrong!');
+    }
+}
+
+viewerLevel();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -59,16 +83,39 @@ let personalMovieDB = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    let lastMovie  = prompt('One of your last watched movie', ''),
-        lastMovieMark = prompt('Rate it from 0 to 10', '');
-    if (lastMovie == '' || !lastMovie || lastMovie.length > 50 || lastMovieMark == '' || !lastMovieMark) {
-        i--;
-    } else {
-        personalMovieDB.movies[lastMovie] = lastMovieMark;
+function lastWatchedMovies() {
+    for (let i = 0; i < 2; i++) {
+        let lastMovie  = prompt('One of your last watched movie', ''),
+            lastMovieMark = prompt('Rate it from 0 to 10', '');
+        if (lastMovie == '' || !lastMovie || lastMovie.length > 50 || lastMovieMark == '' || !lastMovieMark) {
+            i--;
+        } else {
+            personalMovieDB.movies[lastMovie] = lastMovieMark;
+        }
     }
-    
 }
 
-console.log(personalMovieDB.movies);
+lastWatchedMovies();
+
+function writeYourGenres() {
+    for(let i = 0; i < 3; i++) {
+        let num = i + 1;
+        let favoriteGenre = prompt(`What is your favorite genre of movies number ${num}?`, '');
+        if (favoriteGenre == '' || !favoriteGenre ) {
+            i--;
+        } else {
+            personalMovieDB.genres.push(favoriteGenre);
+        }
+    }
+}
+
+writeYourGenres();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
 
